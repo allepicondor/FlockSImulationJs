3798742bf6a40f25a81205d73a8121908103730f
allepicondor <allepiccondor@gmail.com>
2020-11-11 09:03:54 -0600

    kfdsfds

--- sketch.js
+++ sketch.js
@@ -11,59 +11,71 @@ let indexOfMakingObstacle = -1
 function setup(){
     createCanvas(1200, 800)
     alignlabel = createDiv('Align Strength');
-    alignlabel.position(10,height + 10);  
+    alignlabel.style('color','#FFFFFF')
+    //alignlabel.position(10,height + 10);  
     alignSlider = createSlider(0,3,1,0.01);
     alignSlider.parent(alignlabel);
 
     AlignViewDistancelabel = createDiv('AlignViewDistance');
-    AlignViewDistancelabel.position(10,height + 30);  
+    AlignViewDistancelabel.style('color','#FFFFFF')
+    //AlignViewDistancelabel.position(10,height + 30);  
     AlignViewDistanceSlider = createSlider(0,1000,50,10);
     AlignViewDistanceSlider.parent(AlignViewDistancelabel);
 
     FlockCount = createInput(250);
-    FlockCount.position(600,height - 3);
+    //FlockCount.position(600,height - 3);
 
     cohesionlabel = createDiv('Cohesion Strength');
-    cohesionlabel.position(10,height + 60);  
+    cohesionlabel.style('color','#FFFFFF')
+    //cohesionlabel.position(10,height + 60);  
     cohesionSlider = createSlider(0,3,1,0.01);
     cohesionSlider.parent(cohesionlabel);
 
     cohesionDistancelabel = createDiv('cohesionViewDistance');
-    cohesionDistancelabel.position(10,height + 80);  
+    cohesionDistancelabel.style('color','#FFFFFF')
+    //cohesionDistancelabel.position(10,height + 80);  
     cohesionDistanceSlider = createSlider(0,1000,100,10);
     cohesionDistanceSlider.parent(cohesionDistancelabel);
 
     separationlabel = createDiv('Separation Strength');
-    separationlabel.position(10,height + 110);  
+    separationlabel.style('color','#FFFFFF')
+    //separationlabel.position(10,height + 110);  
     separationSlider = createSlider(0,3,1.2,0.01);
     separationSlider.parent(separationlabel);
 
     separationDistancelabel = createDiv('separationViewDistance');
-    separationDistancelabel.position(10,height + 130);  
+    separationDistancelabel.style('color','#FFFFFF')
+    //separationDistancelabel.position(10,height + 130);  
     separationDistanceSlider = createSlider(0,1000,50,10);
     separationDistanceSlider.parent(separationDistancelabel);
 
     maxSpeedlabel = createDiv('Max Speed');
-    maxSpeedlabel.position(10,height + 160);  
+    maxSpeedlabel.style('color','#FFFFFF')
+    //maxSpeedlabel.position(10,height + 160);  
     maxSpeedSlider = createSlider(0,20,4,1);
     maxSpeedSlider.parent(maxSpeedlabel);
 
     maxForcelabel = createDiv('Max Force');
-    maxForcelabel.position(10,height + 180);  
+    maxForcelabel.style('color','#FFFFFF')
+    //maxForcelabel.position(10,height + 180);  
     maxForceSlider = createSlider(0,3,1,0.1);
     maxForceSlider.parent(maxForcelabel);
 
     FollowMouse = createCheckbox('Follow Mouse', false);
-    FollowMouse.position(10, height-3)
+    FollowMouse.style('color','#FFFFFF')
+    //FollowMouse.position(10, height-3)
     AvoidMouse = createCheckbox('Avoid Mouse', false);
-    AvoidMouse.position(150, height-3)
+    AvoidMouse.style('color','#FFFFFF')
+    //AvoidMouse.position(150, height-3)
     AvoidWalls = createCheckbox('Avoid Walls', false);
-    AvoidWalls.position(250, height-3)
+    AvoidWalls.style('color','#FFFFFF')
+    //AvoidWalls.position(250, height-3)
     PlaceObstacle = createCheckbox('Place Obstacles', false);
-    PlaceObstacle.position(350, height-3)
+    PlaceObstacle.style('color','#FFFFFF')
+    //PlaceObstacle.position(350, height-3)
 
     ClearObstacles = createButton('ObstacleClear');
-    ClearObstacles.position(500, height-3);
+    //ClearObstacles.position(500, height-3);
     ClearObstacles.mousePressed(function changeBG(){
         obstacles = []
     });
